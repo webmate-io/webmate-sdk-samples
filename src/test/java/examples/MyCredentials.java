ea86aafb3211da20df1bb1b7aa4d5f71470d3fdc
fredericOfTestfabrik <fburgard@testfabrik.com>
2019-11-28 12:59:12 +0100

    Corrected Port

--- src/test/java/examples/MyCredentials.java
+++ src/test/java/examples/MyCredentials.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 public class MyCredentials {
 
-    public static String WEBMATE_SELENIUM_URL = "https://app.webmate.io:4444/wd/hub";
+    public static String WEBMATE_SELENIUM_URL = "https://app.webmate.io:44444/wd/hub";
     public static String MY_WEBMATE_USERNAME = "xxxx@xxxxxxxxx.com";
     public static String MY_WEBMATE_APIKEY = "xxxxxx-xxxxx-xxxx-ba43-da86b97734eb";
     public static ProjectId MY_WEBMATE_PROJECTID = new ProjectId(UUID.fromString("xxxxxx-1b49-4eb0-bb3a-xxxxxxxxx"));
